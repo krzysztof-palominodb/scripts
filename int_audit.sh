a3b24bac3fbca57fd0c131e8122385c7e6c2f907
Krzysztof Ksiazek <krzysztof@palominodb.com>
2013-02-07 11:56:50 +0000

    tests with db31

--- int_audit.sh
+++ int_audit.sh
@@ -2,7 +2,12 @@
 
 limit=$1
 
-for db in `~/palominodb/scripts/show_nodes_in_cluster.sh | awk -F, '{print $4}'`
+
+~/dba/pdb-check-maxvalue.sh -s $limit -i db31 
+
+
+for db in `~/palominodb/scripts/show_nodes_in_cluster.sh | awk -F, '{print $4}' | grep db31`
+#for db in `~/palominodb/scripts/show_nodes_in_cluster.sh | awk -F, '{print $4}'`
 do
-  ~/dba/pdb-check-maxvalue.sh -s $limit -i $db
+  ~/dba/pdb-check-maxvalue.sh -s $limit -i $db -k
 done
