fbc27940c14bf599cf45a6061675647583b6658f
Krzysztof Ksiazek <krzysztof@palominodb.com>
2013-02-13 16:02:58 +0000

    rewrite and celanup

--- int_audit.py
+++ int_audit.py
@@ -32,37 +32,21 @@ class Threads(threading.Thread):
 
     def run(self):
         output = []
-        dic = {}
         while True:
+            dic = {}
+
             host = self.queue.get()
-            print self.threadid
+            #print self.threadid
             
             p = subprocess.Popen(['/home/kksiazek/dba/pdb-check-maxvalue.sh', '-b', '10250', '-s', str(self.limit), '-i', host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = p.communicate()
             dic[host] = out
             output.append(dic)
-#            print output
             threadoutput[self.threadid] = output
             self.queue.task_done()
 
-
-
-#def process_tasks(args):
-  #  pool = threadpool.ThreadPool(4)
-
- 
-    #let threadpool format your requests into a list
- #   requests = threadpool.makeRequests(long_process, args)
- 
-    #insert the requests into the threadpool
-#    for req in requests:
-#        pool.putRequest(req) 
- 
-#    #wait for them to finish (or you could go and do something else)
-#    pool.wait()
  
 if __name__ == '__main__': 
-#    process_tasks()
     dblist = get_db_list()
     dblist = ['db38', 'db42']
     limit = 2 # limit for pdb-check-maxvalue.sh
@@ -80,7 +64,6 @@ if __name__ == '__main__':
 
     queue.join()
     print "Elapsed Time: %s" % (time.time() - start)
-#    print threadoutput
 
     for arr in threadoutput:
         if (len(arr) > 0):
@@ -91,8 +74,4 @@ if __name__ == '__main__':
                     for line in v.split('\n'):
                         if ("Progress" not in line or "problem" in line):
                             print line
-#                    print v
-            #for line in arr[1].split('\n'):
-                #if ("Progress" not in line or "problem" in line):
-                #    print line
             
